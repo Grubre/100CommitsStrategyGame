35a514e568cebbbd8c208535a92675e996ae60b0
Grubre <ogrodowczykjakub@gmail.com>
2024-03-02 10:44:18 +0100

    Added entt library

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <entt.hpp>
 
-auto main() -> int {
-    std::cout << "Hello World!" << std::endl;
+int main() {
+    entt::registry registry;
+
+    auto entity = registry.create();
+    registry.emplace<std::string>(entity, "Hello, World!");
+
+    const auto& message = registry.get<std::string>(entity);
+    std::cout << message << std::endl;
+
     return 0;
 }
+
