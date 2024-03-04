4b17eaca6e5f9f516ab404ff2cca31ebb04afd8d
Grubre <ogrodowczykjakub@gmail.com>
2024-03-04 17:55:00 +0100

    Added raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <entt.hpp>
+#include "raylib.h"
 
-int main() {
+auto main() -> int {
+    const int screen_width = 800;
+    const int screen_height = 450;
+    const int fps = 60;
+
     entt::registry registry;
+
 
     auto entity = registry.create();
     registry.emplace<std::string>(entity, "Hello, World!");
 
-    const auto& message = registry.get<std::string>(entity);
-    std::cout << message << std::endl;
+    InitWindow(screen_width, screen_height, "Hello World!");
+
+    SetTargetFPS(fps);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText(registry.get<std::string>(entity).c_str(), 190, 200, 20, BLACK);
+        EndDrawing();
+    }
 
     return 0;
 }
-
