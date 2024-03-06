68e5fa43ad06bbad8dd7f64be623a199054b484d
Grubre <ogrodowczykjakub@gmail.com>
2024-03-06 12:19:56 +0100

    Created basic 3d demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
-#include <entt.hpp>
 #include "raylib.h"
+#include "raymath.h"
+#include <entt.hpp>
+#include <iostream>
+
+using Velocity = Vector3;
+
+void update_camera_position(entt::registry &registry) {
+    auto view = registry.view<Camera3D, Velocity>();
+    for (auto entity : view) {
+        auto &velocity = view.get<Velocity>(entity);
+        auto &camera = view.get<Camera3D>(entity);
+        camera.position = Vector3Add(camera.position, {velocity.x, velocity.y, 0});
+    }
+}
 
 auto main() -> int {
     const int screen_width = 800;
@@ -9,9 +21,17 @@ auto main() -> int {
 
     entt::registry registry;
 
+    Camera3D camera = {0};
+    camera.position = (Vector3){0.0f, 10.0f, 10.0f};
+    camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+    camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
 
-    auto entity = registry.create();
-    registry.emplace<std::string>(entity, "Hello, World!");
+    Vector3 cubePosition = {0.0f, 0.0f, 0.0f};
+
+    auto camera_entity = registry.create();
+    registry.emplace<Camera3D>(camera_entity, camera);
 
     InitWindow(screen_width, screen_height, "Hello World!");
 
@@ -19,8 +39,20 @@ auto main() -> int {
 
     while (!WindowShouldClose()) {
         BeginDrawing();
+
         ClearBackground(RAYWHITE);
-        DrawText(registry.get<std::string>(entity).c_str(), 190, 200, 20, BLACK);
+
+        BeginMode3D(camera);
+
+        DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+        DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+
+        DrawGrid(10, 1.0f);
+
+        EndMode3D();
+
+        DrawFPS(10, 10);
+
         EndDrawing();
     }
 
